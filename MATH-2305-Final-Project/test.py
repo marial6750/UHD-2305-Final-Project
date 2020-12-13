ad4685e74f3d44b1aab290344c8ceec8bf7212d0
marial6750 <75641837+marial6750@users.noreply.github.com>
2020-12-12 22:55:13 -0600

    Update test.py

--- MATH-2305-Final-Project/test.py
+++ MATH-2305-Final-Project/test.py
@@ -1,16 +1,16 @@
 import networkx as nx
-from functions import *
-from algorithms import prims_algorithm
+from functions import * # importing all helper functions from functions
+from algorithms import prims_algorithm # from algorithms got prims_algorithm
 
-graph_data = open('test-graph/G2.txt', 'r')
+graph_data = open('test-graph/G2.txt', 'r') # reading what's in G1 file 
 
-G = nx.read_weighted_edgelist(graph_data, nodetype= int)
+G = nx.read_weighted_edgelist(graph_data, nodetype= int) # create a graph 
 
 T = initialize_prims(G,3)
 
-print(f'The vertex of  T is V(T) = {V(T)}')
+print(f'The vertex of  T is V(T) = {V(T)}') # Will print all vertices of the graph 
 print('')
-print(f'The edge of  T is E(T) = {E(T)}')
+print(f'The edge of  T is E(T) = {E(T)}') # Will print all edges of the graph 
 
 #T = prims_algorithm(G, 0)
 
